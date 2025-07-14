24f2348da44a1ed0607ab1522d9024fe508d9247
NoahFuentes <noahmlfuentes@gmail.com>
2025-07-13 19:18:38 -0500

    UI header done

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -38,13 +38,21 @@ public class PlayerInventory : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) addResource("food", 15);
-        if (Input.GetKeyDown(KeyCode.Alpha2)) addResource("water", 15);
-        if (Input.GetKeyDown(KeyCode.Alpha3)) addResource("wood", 10);
-        if (Input.GetKeyDown(KeyCode.Alpha4)) addResource("textile", 10);
-        if (Input.GetKeyDown(KeyCode.Alpha5)) addResource("stone", 5);
-        if (Input.GetKeyDown(KeyCode.Alpha6)) addResource("iron", 5);
-        if (Input.GetKeyDown(KeyCode.Alpha7)) addResource("gold", 3);
+        if (Input.GetKeyDown(KeyCode.Alpha1)) addResource("water", resCountMaxes[0]);
+        if (Input.GetKeyDown(KeyCode.Alpha2)) addResource("food", resCountMaxes[1]);
+        if (Input.GetKeyDown(KeyCode.Alpha3)) addResource("timber", resCountMaxes[2]);
+        if (Input.GetKeyDown(KeyCode.Alpha4)) addResource("rough stone", resCountMaxes[3]);
+        if (Input.GetKeyDown(KeyCode.Alpha5)) addResource("copper ore", resCountMaxes[4]);
+        if (Input.GetKeyDown(KeyCode.Alpha6)) addResource("iron ore", resCountMaxes[5]);
+        if (Input.GetKeyDown(KeyCode.Alpha7)) addResource("gold ore", resCountMaxes[6]);
+        if (Input.GetKeyDown(KeyCode.Alpha8)) addResource("blood shard", resCountMaxes[7]);
+        if (Input.GetKeyDown(KeyCode.Alpha9)) addResource("geode", resCountMaxes[8]);
+        if (Input.GetKeyDown(KeyCode.Alpha0)) addResource("graven scrap", resCountMaxes[9]);
+        if (Input.GetKeyDown(KeyCode.Y)) addResource("yellow essence", resCountMaxes[10]);
+        if (Input.GetKeyDown(KeyCode.U)) addResource("blue essence", resCountMaxes[11]);
+        if (Input.GetKeyDown(KeyCode.I)) addResource("red essence", resCountMaxes[12]);
+        if (Input.GetKeyDown(KeyCode.O)) addResource("white essence", resCountMaxes[13]);
+        if (Input.GetKeyDown(KeyCode.P)) addResource("artifact", resCountMaxes[14]);
     }
 
     
