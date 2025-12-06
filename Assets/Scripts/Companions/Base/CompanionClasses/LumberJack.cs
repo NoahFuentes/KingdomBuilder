113987747a330fc71d9258e8dc2106c3d39b0ce3
Noah Fuentes <noahmlfuentes@gmail.com>
2025-12-06 17:13:02 -0600

    miner functionality

---
+++ Assets/Scripts/Companions/Base/CompanionClasses/LumberJack.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using TMPro;
+
+public class LumberJack : Companion
+{
+    [SerializeField] private float timberInterval;
+    [SerializeField] private float lumberInterval;
+    [SerializeField] private float fineLumberChance;
+    private float currWaitTime;
+    private float timeSenseLastCut;
+
+
+    [SerializeField] private bool processingLumber = false;
+
+    [SerializeField] private TextMeshProUGUI toggleBtnTxt;
+    [SerializeField] private string processLumberTxt;
+    [SerializeField] private string cutTimberTxt;
+    public override void PrimaryInteraction() 
+    {
+        processingLumber = !processingLumber;
+        SetCuttingValues();
+    }
+    public override void SecondInteraction() { }
+
+    private void SetCuttingValues()
+    {
+        timeSenseLastCut = Time.time;
+        if (processingLumber)
+        {
+            toggleBtnTxt.text = cutTimberTxt;
+            currWaitTime = lumberInterval;
+        }
+        else
+        {
+            toggleBtnTxt.text = processLumberTxt;
+            currWaitTime = timberInterval;
+        }
+    }
+
+    //UNITY FUNCTIONS
+
+    public override void Start()
+    {
+        base.Start();
+        SetCuttingValues();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if((stateMachine.CurrentState != atHome) && (Time.time - timeSenseLastCut >= currWaitTime))
+        {
+            if (processingLumber)
+            {
+                if (!KingdomStats.Instance.CanAfford(new string[] { "timber" }, new int[] { 2 })) return;
+                KingdomStats.Instance.RemoveResources(new string[] { "timber" }, new int[] { 2 });
+
+                string[] res = { "lumber" };
+                if (Random.Range(0f, 1f) <= fineLumberChance) res = new string[] { "fine lumber" };
+                KingdomStats.Instance.AddResources(res, new int[] { 1 });
+            }
+            else
+            {
+                string[] res = { "timber" };
+                KingdomStats.Instance.AddResources(res, new int[] { 1 });
+            }
+
+            timeSenseLastCut = Time.time;
+        }
+    }
+}
---
+++ Assets/Scripts/Companions/Base/CompanionClasses/Miner.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using TMPro;
+
+public class Miner : Companion
+{
+
+    [SerializeField] private string[] mineSelections;
+    private int currentMineSelection = 0;
+    [SerializeField] private float[] waitTimes;
+    private float currWaitTime;
+    private float timeSenseLastMine;
+
+    [SerializeField] private string baseInteractionString;
+    [SerializeField] private TextMeshProUGUI toggleBtnTxt;
+    public override void PrimaryInteraction()
+    {
+        currentMineSelection++;
+        if(currentMineSelection >= mineSelections.Length) currentMineSelection = 0;
+        SetMiningValues();
+    }
+    public override void SecondInteraction() { }
+    private string NextSelection()
+    {
+        int nextIndex = currentMineSelection + 1;
+        if(nextIndex >= mineSelections.Length) nextIndex = 0;
+        return mineSelections[nextIndex];
+    }
+
+    private void SetMiningValues()
+    {
+        timeSenseLastMine = Time.time;
+        toggleBtnTxt.text = baseInteractionString + " " + NextSelection() + ".";
+        currWaitTime = waitTimes[currentMineSelection];
+    }
+
+    //UNITY FUNCTIONS
+
+    public override void Start()
+    {
+        base.Start();
+        SetMiningValues();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if ((stateMachine.CurrentState != atHome) && (Time.time - timeSenseLastMine >= currWaitTime))
+        {
+            string[] res = { mineSelections[currentMineSelection] };
+            KingdomStats.Instance.AddResources(res, new int[] { 1 });
+            timeSenseLastMine = Time.time;
+        }
+    }
+}
