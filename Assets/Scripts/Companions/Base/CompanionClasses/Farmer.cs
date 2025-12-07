e23c039e3d2f760b38e1a5fc27ce2b0b979e3992
Noah Fuentes <noahmlfuentes@gmail.com>
2025-12-06 23:34:25 -0600

    farmer functionality

---
+++ Assets/Scripts/Companions/Base/CompanionClasses/Farmer.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Farmer : Companion
+{
+
+    [SerializeField] private float foodTime;
+    [SerializeField] private float clothTime;
+    [SerializeField] private float leatherTime;
+    private float timeSenseLastFood;
+    private float timeSenseLastCloth;
+    private float timeSenseLastLeather;
+
+    private void SetFarmValues()
+    {
+        timeSenseLastFood = Time.time;
+        timeSenseLastCloth = Time.time;
+        timeSenseLastLeather = Time.time;
+    }
+
+    //UNITY FUNCTIONS
+
+    public override void Start()
+    {
+        base.Start();
+        SetFarmValues();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if (stateMachine.CurrentState == atHome) return;
+        if (Time.time - timeSenseLastFood >= foodTime)
+        {
+            string[] res = { "food" };
+            KingdomStats.Instance.AddResources(res, new int[] { 1 });
+            timeSenseLastFood = Time.time;
+        }
+        if (Time.time - timeSenseLastCloth >= clothTime)
+        {
+            string[] res = { "cloth" };
+            KingdomStats.Instance.AddResources(res, new int[] { 1 });
+            timeSenseLastCloth = Time.time;
+        }
+        if (Time.time - timeSenseLastLeather >= leatherTime)
+        {
+            string[] res = { "leather" };
+            KingdomStats.Instance.AddResources(res, new int[] { 1 });
+            timeSenseLastLeather = Time.time;
+        }
+    }
+}
